bfe6b5ce39e0583903a623ffb7b8b38ed2873f81
양희범 <ay612@dsm.hs.kr>
2024-12-30 08:08:11 +0900

    깃허브용 yml & code

--- Decofolio/src/main/java/com/example/decofolio/global/s3/S3Config.java
+++ Decofolio/src/main/java/com/example/decofolio/global/s3/S3Config.java
@@ -4,16 +4,24 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
 
-    private final String accessKey = "ncp_iam_BPAMKRTv7SxDriwViICY";
-    private final String secretKey = "ncp_iam_BPKMKR42wzQDcpP2os3KJiX4gxN2f9E8qi";
-    private final String region = "ap-northeast-2";
-    private final String endpoint = "https://kr.object.ncloudstorage.com";
+    @Value("${AWS_ACCESS_KEY}")
+    private String accessKey;
+
+    @Value("${AWS_SECRET_KEY}")
+    private String secretKey;
+
+    @Value("${AWS_REGION:ap-northeast-2}")
+    private String region;
+
+    @Value("${AWS_S3_ENDPOINT:https://kr.object.ncloudstorage.com}")
+    private String endpoint;
 
     @Bean
     public AmazonS3 amazonS3() {
