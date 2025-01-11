8a59309711927308ea4c5ef1bf307448e420eee9
양희범 <ay612@dsm.hs.kr>
2025-01-11 16:11:46 +0900

    FIX : 커밋용 메시지

--- Decofolio/src/main/java/com/example/decofolio/global/s3/S3Config.java
+++ Decofolio/src/main/java/com/example/decofolio/global/s3/S3Config.java
@@ -10,13 +10,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-    private final String accessKey = "ncp_iam_BPAMKRTv7SxDriwViICY";
-    private final String secretKey = "ncp_iam_BPKMKR42wzQDcpP2os3KJiX4gxN2f9E8qi";
+    private final String accessKey = System.getenv("AWS_ACCESS_KEY");  // 환경 변수로 대체
+    private final String secretKey = System.getenv("AWS_SECRET_KEY");  // 환경 변수로 대체
     private final String region = "ap-northeast-2";
     private final String endpoint = "https://kr.object.ncloudstorage.com";
 
     @Bean
     public AmazonS3 amazonS3() {
+        if (accessKey == null || secretKey == null) {
+            throw new IllegalStateException("AWS credentials are not set in environment variables.");
+        }
+
         BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
         return AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
